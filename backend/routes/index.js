128f25f1b73a6bb49774761781c4f996ee4b3be8
s2172376321 <s2172376321@gmail.com>
2025-03-10 11:01:39 +0800

    Update index.js
    更新：註冊新增的dataRoutes路由

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,11 +7,13 @@ const workLogRoutes = require('./workLogRoutes');
 const noticeRoutes = require('./noticeRoutes');
 const userRoutes = require('./userRoutes');
 const statsRoutes = require('./statsRoutes');
+const dataRoutes = require('./dataRoutes'); // 新增的資料路由
 
 router.use('/auth', authRoutes);
 router.use('/work-logs', workLogRoutes);
 router.use('/notices', noticeRoutes);
 router.use('/users', userRoutes);
 router.use('/stats', statsRoutes);
+router.use('/data', dataRoutes); // 註冊資料路由
 
 module.exports = router;
