66ad876773e93453d1d9423e420b4fc1df494d17
s2172376321 <s2172376321@gmail.com>
2025-03-12 10:27:39 +0800

    Update WorkLogStats.js
    修正工作日誌表單的顯示邏輯

--- frontend/src/components/worklog/WorkLogStats.js
+++ frontend/src/components/worklog/WorkLogStats.js
@@ -1,3 +1,5 @@
+// 位置：frontend/src/components/worklog/WorkLogStats.js
+
 import React, { useState, useEffect } from 'react';
 import { Card } from '../ui';
 import { getTodayHour } from '../../utils/api';
